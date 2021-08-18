cfe6dadf50c5176ab958b48ea43816ae46fb2784
Kiril Rumyantsev <kiril.rumyantsev228@gmail.com>
2021-08-18 10:17:37 +0300

    done

--- src/InchCalculator.java
+++ src/InchCalculator.java
@@ -2,7 +2,7 @@ public class InchCalculator {
 
     public double[] calculate(int min, int max) {
         double[] centiArr = new double[max + 1];
-        double ONE_INCH = 2.54;
+        final double ONE_INCH = 2.54;
         for (int i = min; i < centiArr.length; i++) {
             centiArr[i] = ONE_INCH * i;
         }
